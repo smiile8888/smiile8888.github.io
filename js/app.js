c9ee014efa19563ba338cd969658e3aab6100b07
Smile Thanapattheerakul <smiile@Smiles-MacBook-Pro.local>
2020-05-29 03:09:09 -0400

    Update content - Update images - Implement hamburger to toggle menus - Update bunch of style

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 // Tabbed Menu
 function openMenu(evt, menuName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("menu");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
-    }
-    document.getElementById(menuName).style.display = "block";
-    evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+}
+
+// Toggle Menu
+function toggleMenu() {
+  var x = document.querySelector("#topnav");
+  x.classList.toggle("responsive");
+  // if (x.className === "topnav") {
+  //   x.className += " responsive";
+  // } else {
+  //   x.className = "topnav";
+  // }
 }
